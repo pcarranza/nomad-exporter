a309a9e20f8deab49aaa51c210b87143e0ec2949
Olivier Bourdon <olivier.bourdon@squarescale.com>
2019-10-08 12:41:11 +0200

    Add support for Nomad standard environment variables
    The environment variables are used as default if set
    and can be superseded by command line arguments if needed.

--- args.go
+++ args.go
@@ -1,6 +1,7 @@
 package main
 
 import "flag"
+import "os"
 
 type args struct {
 	ShowVersion                     bool
@@ -39,23 +40,32 @@ func parseArgs() args {
 	flag.StringVar(&a.MetricsPath,
 		"web.telemetry-path", "/metrics", "Path under which to expose metrics.")
 
+	nomad_addr := os.Getenv("NOMAD_ADDR")
+	if len(nomad_addr) == 0 {
+		nomad_addr = "http://localhost:4646"
+	}
 	flag.StringVar(&a.NomadAddress,
-		"nomad.address", "http://localhost:4646", "HTTP API address of a Nomad server or agent.")
+		"nomad.address", nomad_addr, "HTTP API address of a Nomad server or agent.")
 	flag.IntVar(&a.NomadTimeout,
 		"nomad.timeout", 500, "HTTP read timeout when talking to the Nomad agent. In milliseconds")
 	flag.IntVar(&a.NomadWaitTime,
 		"nomad.waittime", 10, "Timeout to wait for the Nomad agent to deliver fresh data. In milliseconds.")
 
+	tls_ca_file := os.Getenv("NOMAD_CACERT")
 	flag.StringVar(&a.TLSCaFile,
-		"tls.ca-file", "", "ca-file path to a PEM-encoded CA cert file to use to verify the connection to nomad server")
+		"tls.ca-file", tls_ca_file, "ca-file path to a PEM-encoded CA cert file to use to verify the connection to nomad server")
+	tls_ca_path := os.Getenv("NOMAD_CAPATH")
 	flag.StringVar(&a.TLSCaPath,
-		"tls.ca-path", "", "ca-path is the path to a directory of PEM-encoded CA cert files to verify the connection to nomad server")
+		"tls.ca-path", tls_ca_path, "ca-path is the path to a directory of PEM-encoded CA cert files to verify the connection to nomad server")
+	tls_cert_file := os.Getenv("NOMAD_CLIENT_CERT")
 	flag.StringVar(&a.TLSCert,
-		"tls.cert-file", "", "cert-file is the path to the client certificate for Nomad communication")
+		"tls.cert-file", tls_cert_file, "cert-file is the path to the client certificate for Nomad communication")
+	tls_cert_key := os.Getenv("NOMAD_CLIENT_KEY")
 	flag.StringVar(&a.TLSKey,
-		"tls.key-file", "", "key-file is the path to the key for cert-file")
+		"tls.key-file", tls_cert_key, "key-file is the path to the key for cert-file")
+	tls_skip_verify := os.Getenv("NOMAD_SKIP_VERIFY")
 	flag.BoolVar(&a.TLSInsecure,
-		"tls.insecure", false, "insecure enables or disables SSL verification")
+		"tls.insecure", len(tls_skip_verify) > 0, "insecure enables or disables SSL verification")
 	flag.StringVar(&a.TLSServerName,
 		"tls.tls-server-name", "", "tls-server-name sets the SNI for Nomad ssl connection")
 
